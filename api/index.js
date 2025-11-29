ee71b2d8f0bcb72606da330cb0c885414e0a9992
Purnav Bhatt <purnavbhatt@gmail.com>
2025-11-29 23:20:15 +0530

    chaneg location of index.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const connectDB = require('./config/db');
-const errorHandler = require('./middleware/errorHandler');
+const connectDB = require('../config/db');
+const errorHandler = require('../middleware/errorHandler');
 
 // Connect DB
 connectDB();
@@ -16,9 +16,9 @@ app.use(morgan('dev'));
 app.use(cors({ origin: process.env.CLIENT_URL || '*' }));
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/expenses', require('./routes/expenses'));
-app.use('/api/categories', require('./routes/categories'));
+app.use('/api/auth', require('../routes/auth'));
+app.use('/api/expenses', require('../routes/expenses'));
+app.use('/api/categories', require('../routes/categories'));
 
 // Error handler
 app.use(errorHandler);
